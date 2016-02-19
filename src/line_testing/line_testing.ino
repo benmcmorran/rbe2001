74bd30e569db9a83cd57cffa949a13bb796dba33
Ben McMorran <ben.mcmorran@gmail.com>
2016-02-19 00:01:14 -0500

    Line tracking adjustments

--- src/line_testing/line_testing.ino
+++ src/line_testing/line_testing.ino
@@ -1,6 +1,7 @@
 #include "LineSensor.h"
 #include <Servo.h>
 #include <PID_v1.h>
+#include <TimerOne.h>
 
 #define LED_PIN 22
 
@@ -16,24 +17,27 @@ Servo left, right;
 int leftSpeed, rightSpeed;
 
 double Setpoint, Input, Output;
-PID myPID(&Input, &Output, &Setpoint, 20, 0, 0, DIRECT);
+PID myPID(&Input, &Output, &Setpoint, 30, 0, 1, DIRECT);
 
 void setup() {
   Serial.begin(9600);
-  left.attach(11);
-  right.attach(10);
+  left.attach(10);
+  right.attach(11);
   pinMode(LED_PIN, OUTPUT);
 
   Input = 0;
   Setpoint = 0;
   myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255); 
 }
 
+int loopCount = 0;
 void loop() {
   digitalWrite(LED_PIN, isInIntersection);
   
   sensor.update();
   int averageReading = sensor.averageReading();
+  
   if (isInIntersection && averageReading < negativeIntsersectionThreshold) {
     isInIntersection = false;
   } else if (!isInIntersection && averageReading > positiveIntersectionThreshold) {
@@ -41,21 +45,26 @@ void loop() {
     intersectionCount++;
   }
 
-  if (intersectionCount == 5) {
+  /*if (intersectionCount == 4) {
     leftSpeed = 90;
     rightSpeed = 90;
-  } else if (sensor.maxReading() > 200) {
-    float linePos = sensor.averageLinePosition();
-    leftSpeed = 110 - (10 * linePos);
-    rightSpeed = 110 + (10 * linePos);
-  }
-  /*else {
+  } else*/ if (sensor.maxReading() > 200 && !isInIntersection) {
     Input = sensor.averageLinePosition();
     myPID.Compute();
-    leftSpeed = 100 + Output;
-    rightSpeed = 100 - Output;
-  }*/
+    leftSpeed = 85 + Output;
+    rightSpeed = 85 - Output;
+  }
 
-  left.write(leftSpeed);
-  right.write(rightSpeed);
+  safeMotorDrive(&left, leftSpeed);
+  safeMotorDrive(&right, rightSpeed);
+
+  loopCount++;
+  if (loopCount == 10000) {
+    Serial.println(millis());
+  }
 }
+
+void safeMotorDrive(Servo *motor, int value) {
+  motor->write(max(0, min(180, value)));
+}
+
